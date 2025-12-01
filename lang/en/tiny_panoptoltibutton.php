09d403ed57580ed9e67f434574dc0403b7fe69db
Alistair Spark <aspark21@users.noreply.github.com>
2025-12-01 10:05:06 +0000

    Add missing lang string

--- lang/en/tiny_panoptoltibutton.php
+++ lang/en/tiny_panoptoltibutton.php
@@ -25,6 +25,7 @@
 
 $string['helplinktext'] = 'PanoptoLTI Button';
 $string['modal_button_close'] = 'Close';
+$string['panoptoltibutton:use'] = 'Use Panopto button';
 $string['panopto_button_description'] = 'Add Panopto Video';
 $string['panopto_button_long_description'] = 'Add Panopto Video Content to TinyMCE';
 $string['panopto_button_unprovisioned_error'] = 'Error: Please use the Panopto Block to provision this course before using this tool';
